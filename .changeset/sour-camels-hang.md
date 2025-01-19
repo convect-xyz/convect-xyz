6656b3f91afddb810c16cfd0249f1533fc10ab62
kayakthedev <120797053+kayakthedev@users.noreply.github.com>
2025-01-19 08:36:43 +0000

    add deployment id to convect fn context

---
+++ .changeset/sour-camels-hang.md
@@ -0,0 +1,5 @@
+---
+'convect-xyz': patch
+---
+
+Add Deployment ID to convect function context
--- src/sdk/lib/transaction.ts
+++ src/sdk/lib/transaction.ts
@@ -68,6 +68,7 @@ export class Transaction<
 
 export type HandlerContext = {
 	chainId: number;
+	deploymentId: string;
 };
 
 type TransactionOptions<
